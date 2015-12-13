1bd81811e5089001d5c258775ae5612831bc2a88
SOFIA <carvalho@StLuke.local>
2015-12-13 21:32:37 +0000

    project 1

--- prog4.R
+++ prog4.R
@@ -13,7 +13,7 @@ y=as.numeric(subdata$Global_active_power)
 plot(x,y,type="l",xlab="",ylab="Global Active Power (kilowatts)")
 ##
 y=as.numeric(subdata$Voltage)
-plot(x,y,type="l",xlab="",ylab="Voltage")
+plot(x,y,type="l",xlab="datetime",ylab="Voltage")
 ##
 y1=as.numeric(subdata$Sub_metering_1)
 y2=as.numeric(subdata$Sub_metering_2)
@@ -24,7 +24,7 @@ points(x,y3,type="l",col="blue")
 legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=1,bty="n")
 ##
 y=as.numeric(subdata$Global_reactive_power)
-plot(x,y,type="l",xlab="",ylab="Global_reactive_power")
+plot(x,y,type="l",xlab="datetime",ylab="Global_reactive_power")
 dev.off()
 
 #par(mfrow=c(1,1))
